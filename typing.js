8aa5ad54ff6d3c61adae8fef280954e92dc3ce56
Ji Feng <jifeng@menlosecurity.com>
2017-12-01 15:24:34 -0800

    Now supports words.

--- typing.js
+++ typing.js
@@ -2,17 +2,22 @@
 var total_press = 0;
 var correct_press = 0;
 var cur_cursor = 0;
-
+var word_typing_correct = true;
+var penalty_displayed = false;
 function get_combo() {
    var possible = ['of', 'to', 'in', 'it', 'is', 'be', 'as', 'at', 'so', 'we',
        'he', 'by', 'or', 'on', 'do', 'if', 'me', 'my', 'up', 'an', 'go', 'no',
        'us', 'am'];
-   possible.concat(['the', 'and', 'for', 'are', 'but', 'not', 'you', 'all', 'any',
-            'can', 'had', 'her', 'was', 'one', 'our', 'out', 'day', 'get', 'has',
-            'him', 'his', 'how', 'man', 'new', 'now', 'old', 'see', 'two', 'way',
-            'who', 'boy', 'did', 'its', 'let', 'put', 'say', 'she', 'too', 'use']);
-   possible.concat(['that', 'with', 'have', 'this', 'will', 'your', 'from', 'they',
-            'know', 'want', 'been', 'good', 'much', 'some', 'time']);
+   possible = possible.concat([
+         'the', 'and', 'for', 'are', 'but', 'not', 'you', 'all', 'any',
+         'can', 'had', 'her', 'was', 'one', 'our', 'out', 'day', 'get', 'has',
+         'him', 'his', 'how', 'man', 'new', 'now', 'old', 'see', 'two', 'way',
+         'who', 'boy', 'did', 'its', 'let', 'put', 'say', 'she', 'too', 'use'
+      ]);
+   possible = possible.concat([
+         'that', 'with', 'have', 'this', 'will', 'your', 'from', 'they',
+         'know', 'want', 'been', 'good', 'much', 'some', 'time'
+      ]);
    text = possible[Math.floor(Math.random() * possible.length)];
    return text;
 }
@@ -70,23 +75,36 @@ function set_new_text(new_text) {
    }
    while (cur_pos < new_text.length) {
       cur_char = new_text[cur_pos];
-      letter_box = document.createElement('DIV');
+      letter_box = document.createElement('span');
       letter_box.textContent = cur_char;
       letter_box.classList.add('letterbox');
       target_box.appendChild(letter_box);
       cur_pos += 1;
    }
+   target_box.parentNode.style.width = (150 * new_text.length) + 'px';
+}
+
+function locate_current_letter_span() {
+   var target_box = document.getElementById('target_box');
+   var children = target_box.childNodes;
+   var child;
+   var cur_letter_span = 0;
+   var i;
+   for (i = 0; i < children.length; i++) {
+      child = target_box.children[i];
+      if (child.nodeName.toLowerCase() === 'span') {
+         if (cur_letter_span === cur_cursor) {
+            return child;
+         } else {
+            cur_letter_span += 1;
+         }
+      }
+   }
 }
 
 function display_hit_effect() {
-   var new_char = get_new_text();
-   var elem = document.getElementById('target_box');
-   elem.classList.add('hidden');
-   setTimeout(function () {
-      elem.classList.remove('hidden');
-      set_new_text(new_char);
-      elem.classList.add('visible');
-   }, 100); // Consistent with the style duration.
+   var letter_box = locate_current_letter_span();
+   letter_box.style.color = 'gray';
 }
 
 function display_miss_effect() {
@@ -103,14 +121,31 @@ function verify_key(char_pressed) {
    console.log('onscreen:' + on_screen);
    if (char_pressed !== on_screen[cur_cursor]) {
       display_miss_effect();
+      word_typing_correct = false;
+         // Display penalty immediately.
+      if (!penalty_displayed) {
+         display_stats(false);
+         penalty_displayed = true;
+      }
    } else {
       display_hit_effect();
       cur_cursor += 1;
       if (cur_cursor >= on_screen.length) {
+         elem.classList.add('hidden');
+            setTimeout(function () {
+               var new_char = get_new_text();
+               elem.classList.remove('hidden');
+               set_new_text(new_char);
+               elem.classList.add('visible');
+            }, 100); // Consistent with the style duration.
          cur_cursor = 0;
+         if (word_typing_correct) {
+            display_stats(word_typing_correct);
+         }
+         word_typing_correct = true;
+         penalty_displayed = false;
       }
    }
-   display_stats(char_pressed === on_screen);
 }
 
 function check_key(evt) {
